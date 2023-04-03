76f19aad272720728b243e29d030df2af8a303c0
Toni Listiyo <l.toni2007@gmail.com>
2023-04-03 09:36:23 +0700

    initial commit

---
+++ src/server.js
@@ -0,0 +1,24 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+const Hapi = require('@hapi/hapi');
+
+const routes = require('./routes');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 8080,
+    host: '127.0.0.1',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  server.route(routes);
+
+  await server.start();
+
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
